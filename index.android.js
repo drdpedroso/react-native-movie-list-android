4427be67f3ea2a461689dba86e1d1ddda5d1ff2c
Eduardo Pedroso <eduardo.pedroso@matera.com>
2016-07-04 19:37:56 -0300

    Adding API request to movies

--- index.android.js
+++ index.android.js
@@ -13,39 +13,93 @@ import {
   View
 } from 'react-native';
 
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
 class NewProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
-    return (
-      <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image source={{uri: movie.posters.thumbnail}} />
-      </View>
-    );
+     if (!this.state.movies) {
+       return this.renderLoadingView();
+     }
+
+     var movie = this.state.movies[0];
+     return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
+   return (
+     <View style={styles.container}>
+       <Text>
+         Loading movies...
+       </Text>
+     </View>
+   );
+  }
+
+  renderMovie(movie) {
+   return (
+     <View style={styles.container}>
+       <Image
+         source={{uri: movie.posters.thumbnail}}
+         style={styles.thumbnail}
+       />
+       <View style={styles.rightContainer}>
+         <Text style={styles.title}>{movie.title}</Text>
+         <Text style={styles.year}>{movie.year}</Text>
+       </View>
+     </View>
+   );
+  }
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
-  instructions: {
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
